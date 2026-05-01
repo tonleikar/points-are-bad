017e2398922c113e2ad20a1bbc98a8379bef06a3
Sam Gorman-Hailey <sam.gormanhailey@pm.me>
2026-05-01 16:00:40 +0100

    add username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  private
+
+  def configure_permitted_parameters
+    # For registration
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
+    # For account updates
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :username ])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_01_145505) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_01_145730) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -19,7 +19,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_01_145505) do
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
