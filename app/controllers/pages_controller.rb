9b8936647d86a74c95d4fe97ab83f6976db891d6
Sam Gorman-Hailey <sam.gormanhailey@pm.me>
2026-05-01 15:52:04 +0100

    add devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @matches = FootballData.fetch_scores(matchday: 35)
+    @matches = FootballData.fetch_games(matchday: 35)
   end
 end
--- app/services/football_data.rb
+++ app/services/football_data.rb
@@ -4,7 +4,18 @@ require "json"
 class FootballData
   BASE_URL = "https://api.football-data.org/v4"
 
-  def self.fetch_scores(matchday: nil)
+  def self.fetch_games(matchday: nil)
+    url = URI("#{BASE_URL}/competitions/PL/matches/#{matchday ? "?matchday=#{matchday}" : ""}")
+
+    response = URI.open(url, "X-Auth-Token" => ENV.fetch("FOOTBALL_DATA_KEY")).read
+    JSON.parse(response)["matches"]
+  rescue StandardError => e
+    { error: "Failed to fetch football data: #{e.message}" }
+  end
+
+  def self.fetch_score(game_id: nil)
+    return { error: "No match ID given" } if game_id == nil
+
     url = URI("#{BASE_URL}/competitions/PL/matches/#{matchday ? "?matchday=#{matchday}" : ""}")
 
     response = URI.open(url, "X-Auth-Token" => ENV.fetch("FOOTBALL_DATA_KEY")).read
