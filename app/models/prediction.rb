4f819181dbc62e3bf46b607d4f6f4d2fc2189eeb
Sam Gorman-Hailey <sam.gormanhailey@pm.me>
2026-05-28 15:53:45 +0100

    fix incorrect symbols in model

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -2,7 +2,7 @@ class Prediction < ApplicationRecord
   belongs_to :user
   belongs_to :match
 
-  validates :predicted_home_score, :predicted_away_score, presence: true
+  validates :home_prediction, :away_prediction, presence: true
   validates :user_id, uniqueness: { scope: :match_id, message: "has already made a prediction for this match" }
 
   # TODO Prediction cannot be 1 - 1. Could be done in the controller.
